cea75fd89cdf211ad73a2e1a5262c66c7fc84e18
Lulca <Anatolkruv@gmail.com>
2016-05-16 11:33:10 +0300

    add bootstrap carousel

--- app/js/libs.js
+++ app/js/libs.js
@@ -1,3 +1,5 @@
 //@prepros-append ../libs/jquery-1.12.3/jquery-1.12.3.js
 //@prepros-append ../libs/bootstrap-3.3.6-dist/js/bootstrap.js
 //@prepros-append ../libs/Magnific-Popup-master/dist/jquery.magnific-popup.js
+//@prepros-append ../libs/superfish/dist/js/superfish.js
+//@prepros-append ../libs/owl.carousel-2/owl.carousel.js
--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,10 @@
 $(function () {
 
+	$('.top-line .sf-menu').superfish({
+		delay: 200
+	});
 
-
+	$('.carousel').carousel({
+		interval: 5000
+	})
 });
