270720fb7e59e57f5b29612661efd7aa92947af9
Lulca <Anatolkruv@gogle.com>
2016-06-02 10:50:37 +0300

    fix bloks interference on top-nav, pos absolute on galery

--- app/js/main.js
+++ app/js/main.js
@@ -1,59 +1,75 @@
+var topNavigation = (function(){
 
+	
+	return {
+
+		init: function() {
+			var that = this;
+
+			$('.portfolio').on('click', function(e) {
+
+				that.toggleMenu($(this));
+
+			});
+
+			$('.portfolio li').on('click',function(e) {
+				e.stopPropagation();
+			});
+
+			var subPortfolio = $('.sub-portfolio');
+
+			subPortfolio.on('click', function(e) {
+				var that = $(this);
+				that.toggleClass('activeSub2Li');
+				that.find('.sub-sub-menu').fadeToggle(200);
+			});
+
+			$(".toggle-mnu").click(function() {
+				$(this).toggleClass("on");
+				$(".main-mnu").fadeToggle(200);
+			});
+
+			$('.toggle-mnu').click(function() {
+				$('.sf-menu').fadeToggle(200);
+			});
+
+			$(window).resize(function() {
+				if($(window).width() >= 992) {
+					$('.sf-menu').fadeIn(1);
+				}
+				if($(window).width() < 992) {
+					$('.sf-menu').fadeOut(1);
+					$('.toggle-mnu').removeClass('on');
+				}
+			});
+		},
+
+
+		toggleMenu: function (that) {
+			if(that.find('.sub-portfolio').hasClass('activeSub2Li')) {
+				that.find('.sub-portfolio').removeClass('activeSub2Li')
+				.find('.sub-sub-menu').fadeToggle(200);
+			}
+			that.toggleClass('activeSubLi');
+
+			if (that.hasClass('activeSubLi')) {
+				that.find('.sub-menu').fadeIn(200);
+			} else {
+				that.find('.sub-menu').fadeOut(1);
+			}
+		}
+
+
+	}
+
+})();
 
 
 
 
 $(function () {
 
-
-	var portfolio = $('.portfolio');
-
-	portfolio.on('click', function(e) {
-		var that = $(this);
-		if(that.find('.sub-portfolio').hasClass('activeSub2Li')) {
-			that.find('.sub-portfolio').removeClass('activeSub2Li')
-			.find('.sub-sub-menu').slideToggle(200);
-		}
-		that.toggleClass('activeSubLi');
-		
-		if (that.hasClass('activeSubLi')) {
-			that.find('.sub-menu').slideDown(200);
-		} else {
-			that.find('.sub-menu').slideUp(1);
-		}
-
-	});
-
-	$('.portfolio li').on('click',function(e) {
-		e.stopPropagation();
-	});
-
-	var subPortfolio = $('.sub-portfolio');
-
-	subPortfolio.on('click', function(e) {
-		var that = $(this);
-		that.toggleClass('activeSub2Li');
-		that.find('.sub-sub-menu').slideToggle(200);
-	});
-
-	$(".toggle-mnu").click(function() {
-		$(this).toggleClass("on");
-		$(".main-mnu").slideToggle(200);
-	});
-
-	$('.toggle-mnu').click(function() {
-		$('.sf-menu').slideToggle(200);
-	});
-
-	$(window).resize(function() {
-		if($(window).width() >= 992) {
-			$('.sf-menu').slideDown(1);
-		}
-		if($(window).width() < 992) {
-			$('.sf-menu').slideUp(1);
-			$('.toggle-mnu').removeClass('on');
-		}
-	});
+	topNavigation.init();
 
 
 	$('.slides').owlCarousel({
@@ -93,5 +109,6 @@ $(function () {
 	// $('.card').on('click', function(){
 	// 	$(this).addClass('rotate');
 	// });
+
 
 });
