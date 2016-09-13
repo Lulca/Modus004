16045227cedc86fb951c7262f9b9a74553458bad
Lulca <Anatolkruv@gmail.com>
2016-09-13 14:55:11 +0300

    withdraw index.html from app/

--- app/js/main.js
+++ app/js/main.js
@@ -15,30 +15,6 @@ var topNavigation = (function(){
 
 			});
 
-		
-
-			// portfolio.on('mouseover', function(){
-			// 	if( $(window).width() > 992) {
-			// 		$this = $(this);
-
-			// 		$this.addClass('activeSubLi').find('.sub-menu').fadeIn(1);
-			// 	}
-
-			// });
-
-			// portfolio.on('mouseleave', function(){
-			// 	if( $(window).width() > 992) {
-			// 		$this = $(this);
-			// 		$this.removeClass('activeSubLi');
-			// 		setTimeout(
-			// 			function() {
-			// 				if(!$this.hasClass('activeSubLi')) {
-			// 					$this.find('.sub-menu').fadeOut(200);	
-			// 				}	
-			// 			},
-			// 			200);		
-			// 	}
-			// });
 
 			$('.portfolio li').on('click',function(e) {
 				e.stopPropagation();
@@ -53,30 +29,6 @@ var topNavigation = (function(){
 				that.toggleClass('activeSub2Li');
 				that.find('.sub-sub-menu').fadeToggle(200);
 			});
-
-			// subPortfolio.on('mouseover', function(){
-			// 	if( $(window).width() > 992) {
-			// 		$this = $(this);
-
-			// 		$this.addClass('activeSub2Li').find('.sub-sub-menu').fadeIn(1);
-			// 	}
-					
-			// });
-
-			// subPortfolio.on('mouseleave', function(){
-			// 	$this = $(this);
-			// 	if( $(window).width() > 992) {
-					
-			// 		$this.removeClass('activeSub2Li');
-			// 		setTimeout(
-			// 			function() {
-			// 				if($this.hasClass('activeSub2Li')) {
-			// 					$this.find('.sub-sub-menu').fadeOut(200);	
-			// 				}	
-			// 			},
-			// 			200);		
-			// 	}
-			// });
 
 			$(".toggle-mnu").click(function() {
 				$(this).toggleClass("on");
@@ -120,64 +72,9 @@ var topNavigation = (function(){
 })();
 
 
-var galery = (function(){
-
-
-	// function move from left-right to center
-
-	function toCenter ($this,degree) {
-		var
-			volumeGalery = $this.closest('.3d-galery');
-			leftItem = volumeGalery.find('.left-item');
-			activeItem = volumeGalery.find('.active-item');
-
-		$this.animate({
-			'-webkit-transform': 'rotateY(' + degree + 'deg)',
-            '-moz-transform': 'rotateY(' + degree + 'deg)',
-            '-ms-transform': 'rotateY(' + degree + 'deg)',
-            '-o-transform': 'rotateY(' + degree + 'deg)',
-            'transform': 'rotateY(' + degree + 'deg)',
-            'zoom': 1,
-			'left': '0px',
-			'z-index': '4',
-			'max-width': '300px'
-
-		}, 1000, function(){
-			
-		});
-	}
-
-	// function mone from center to right-left
-
-
-	return {
-		init: function() {
-
-			var that = this;
-
-			$('.left-item').on('click', function(){
-				$this = $(this);
-				// $this.addClass('left-item-move active-item-move');
-				toCenter($this,0);
-			});
-
-
-
-
-		}
-	}
-
-})();
-
-
-
-
 $(function () {
 
 	topNavigation.init();
-
-	galery.init();
-
 
 	$('.slides').owlCarousel({
 		margin:30,
